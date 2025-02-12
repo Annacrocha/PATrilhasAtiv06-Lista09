32a26d4058bbbd8fa2707e2998be35e24b82fcc0
Anna Rocha <168745681+Annacrocha@users.noreply.github.com>
2025-02-12 08:38:37 -0300

    vetor de inteiros e encontre o menor valor no vetor.

---
+++ Letra E.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace lista09
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("VETOR NÚMERO MENOR\n");
+			
+			int[] numero = new int[4];
+			numero[0] = 15;
+			numero[1] = 47;
+			numero [2] = 2;
+			numero[3] = 10;
+			
+			 int maiorNumero = numero[0];  
+            
+            for (int i = 1; i < numero.Length; i++)  
+            {
+                if (numero[i] < maiorNumero)
+                {
+                    maiorNumero = numero[i];
+                }
+            }
+
+            
+            Console.WriteLine("O menor número no vetor é: " + maiorNumero);
+            Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+	}
+
+		}
+            
+}
